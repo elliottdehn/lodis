dc7466a24410f2cb7220a0466069c0e235d365b7
Elliott Dehnbostel <egd@Elliotts-MacBook-Pro.local>
2022-11-23 06:58:18 -0500

    IT WORKS!

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,11 @@
 /// This is a hard-to-kill web-server, and an easy-to-upgrade binary!
+
 // "Upgrade all hosts" is ~TRIVIAL: `cargo build` (overwrite binary).
-// "Killing all hosts" is ~IMPOSSIBLE (out-of-band kill-all required):
+// >> Use nginx (or any LB) to distribute incoming load across ports.
+
+// "Killing all hosts" is ~IMPOSSIBLE (out-of-band kill-all REQUIRED):
 // >> Use `chmod 000 host` to slay all of the hosted servers at once.
+
 use actix_web::web::Buf;
 use actix_web::{get, App, HttpResponse, HttpServer, Responder};
 use async_std::sync::RwLock;
@@ -24,12 +28,12 @@ use std::process::{abort, id, Command};
 use std::time::{Duration, SystemTime, UNIX_EPOCH};
 use std::{env, io};
 
-#[get("/__/time")]
+#[get("/__/time")] // Always useful.
 async fn _gen_time() -> impl Responder {
   ok(&now())
 }
 
-#[get("/__/kill")] // Good luck!
+#[get("/__/kill")] // "Never useful!"
 async fn _gen_kill() -> impl Responder {
   abort();
   ok(&true) // NOTE: Unreachable.
@@ -40,16 +44,16 @@ async fn main() -> io::Result<()> {
   let argv = env::args().collect::<Vec<String>>();
   let exec = &argv[0];
   let port = argv.get(1).map(|s| s.parse::<Port>().x()).unwrap_or(8080);
-  let card = argv // target cardinality of healthy server distribution.
+  let card = argv // Target cardinality of healthy server distribution.
     .get(2)
     .map(|s| s.parse::<usize>().x())
     .unwrap_or_else(num_cpus::get_physical);
 
-  // Create the server and start listening on nearest available port.
+  // Create this server and start listening on nearest available port!
   let port_deploy = if card > 0 {
-    gen_deploy((port, None)) // Deploy FROM port (with wandering).
+    gen_deploy((port, None)) // Deploy FROM port (may/will wander).
   } else {
-    gen_deploy((port, Some(port))) // Deploy AT port (zero wandering).
+    gen_deploy((port, Some(port))) // Deploy TO port (never wander).
   }
   .await
   .expect("Failed to deploy the binary"); // Many will try; one wins.
@@ -59,11 +63,11 @@ async fn main() -> io::Result<()> {
     let mut exec_init = Command::new(exec);
     exec_init.arg((port_deploy + 1).to_string()); // Wanders from last.
     exec_init.arg((card as u16 - 1).to_string());
-    exec_init.spawn().expect("Failed to spawn the child"); // No block.
-  } // When single-servers get re-deployed, cardinality 0 is provided.
+    exec_init.spawn().expect("Failed to spawn the child"); // No block!
+  } // When single-servers get re-deployed a cardinality 0 is provided.
 
   // EVERY SECOND: The server has a 50% chance to abort itself QUIETLY!
-  // > This is just for demonstration and SHOULD NOT be produced.
+  // >> This is just for demonstration and SHOULD NOT be in production.
   let mut rng = rand::thread_rng();
   loop {
     for host_port in get_host_ports() {
@@ -72,11 +76,13 @@ async fn main() -> io::Result<()> {
           .await;
       match response {
         Ok(_) => {}
-        Err(_) => gen_spawn(host_port).await,
+        Err(_) => /* "Oh dear, you are dead!" */ gen_respawn(host_port)
+          .await,
       }
     }
+    // ...not anymore!
     if rng.gen::<f64>() > 0.5 {
-      abort();
+      abort(); /* "Oh dear, I am dead!" */
     } else {
       task::sleep(Duration::from_secs(1)).await;
     }
@@ -175,7 +181,7 @@ fn get_host_ports() -> HashSet<Port> {
     .collect::<HashSet<Port>>()
 }
 
-async fn gen_spawn(port: Port) {
+async fn gen_respawn(port: Port) {
   let argv = env::args().collect::<Vec<String>>();
   let exec = &argv[0];
   let mut exec_init = Command::new(exec);
